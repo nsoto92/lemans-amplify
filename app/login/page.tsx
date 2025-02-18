ca881a69912b3afd45d2c20191a9ff57e3ab0659
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2025-02-18 16:53:22 -0400

    initial

---
+++ app/login/page.tsx
@@ -0,0 +1,42 @@
+// app/login/page.tsx - Custom <Authenticator>
+
+"use client";
+
+import {
+  Authenticator,
+  Text,
+  View,
+  useAuthenticator,
+} from "@aws-amplify/ui-react";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+const components = {
+  Header() {
+    return (
+      <View textAlign="center">
+        <Text><span style={{color: "white"}}>Authenticator Header</span></Text>
+      </View>
+    );
+  },
+};
+
+function CustomAuthenticator() {
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  useEffect(() => {
+    if (user) {
+      redirect("/");
+    }
+  }, [user]);
+
+  return <Authenticator components={components} />;
+}
+
+export default function Login() {
+  return (
+    <Authenticator.Provider>
+      <CustomAuthenticator />
+    </Authenticator.Provider>
+  );
+}
