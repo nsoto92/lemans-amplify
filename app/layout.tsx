6ec4ac0d7e676f2b0d72e4c1ab75eb44d2b7b9c8
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2025-04-26 01:59:17 -0400

    build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""
+          async
         />
         <meta
           name="viewport"
--- locales/es.ts
+++ locales/es.ts
@@ -6,6 +6,10 @@ export default {
         subtitle: "Auto Detailing Profesional a Domicilio",
         description: "En Le Mans Auto Detailing llevamos el servicio de auto detailing hasta tu puerta. Atendemos todo tipo de vehículos con la más alta calidad, cuidado y dedicación, para que tu auto luzca como nuevo, sin que tengas que salir de casa.",
         button: "Conoce nuestros servicios",
+        map: {
+            title: "Nuestra ubicación",
+            subtitle: "Encuentranos en la zona de San Juan, Puerto Rico",
+        }
     },
     services: {
         title: "Servicios",
